72a3b061cafe979a554deca595e81bd6b67d67ac
Lucifer-Elf <alfred.v.paul@gmail.com>
2023-04-27 00:08:39 +0400

    JWt Added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,18 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/User.js";
-import {BadRequestError} from "../errors/index.js";
+import { BadRequestError } from "../errors/index.js";
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     throw new BadRequestError("Please provide all value");
   }
-const userexist = await User.findOne({email});
-if(userexist)
-    throw new BadRequestError("Email Already in use")
+  const userexist = await User.findOne({ email });
+  if (userexist) throw new BadRequestError("Email Already in use");
 
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user,token });
 };
 const login = async (req, res) => {
   res.send("login user");
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcryt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,8 @@ const UserSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, "Please provide password"],
-    minlength: 6
+    minlength: 6,
+    select:false,
   },
   lastName: {
      type: String,
@@ -44,5 +46,9 @@ UserSchema.pre('save',async function(){
   this.password = await bcryt.hash(this.password,salt)
 
 })
+
+UserSchema.methods.createJWT = function(){
+  return jwt.sign({userId:this._id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME})
+}
 
 export default mongoose.model('User',UserSchema)
